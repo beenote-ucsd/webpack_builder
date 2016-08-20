96a7ef9a6cd98090a7c427e573471ac51db4a648
Xinyu Zhang <sayenzhang@gmail.com>
2016-08-20 12:34:54 -0700

    Update webpack.template.js

--- webpack.template.js
+++ webpack.template.js
@@ -6,9 +6,37 @@ module.exports = {
           filename: 'TODO.js'
         },
     module: {
-            loaders: [
-                { test: path.join(__dirname, 'TODO'),
-                  loader: 'babel-loader' }
-            ]
+            loaders: [{
+            test: /\.js?$/,
+            exclude: [/node_modules/, /__tests__/],
+            loaders: ['babel?presets[]=es2015,presets[]=stage-1,presets[]=react']
+        }, {
+            test: /\.css$/,
+            loaders: 'style-loader!css-loader'
+        }, {
+            test: /\.scss$/,
+            loaders: ["style", "css", "sass"]
+        }, {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file"
+        }, {
+            test: /\.(woff|woff2)$/,
+            loader: "url?prefix=font/&limit=5000"
+        }, {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/octet-stream"
+        }, {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=image/svg+xml"
+        }, {
+            test: /\.gif/,
+            loader: "url-loader?limit=10000&mimetype=image/gif"
+        }, {
+            test: /\.jpg/,
+            loader: "url-loader?limit=10000&mimetype=image/jpg"
+        }, {
+            test: /\.png/,
+            loader: "url-loader?limit=10000&mimetype=image/png"
+        }]
     }
 };
